46316d0b375f0ce5a98b9c8d00aa535794acfa1d
alberto batista <albertomimi16@gmail.com>
2025-02-27 19:27:33 -0400

    Creando el database toolkit

---
+++ config/DatabaseToolkit.php
@@ -0,0 +1,53 @@
+<?php
+
+class DatabaseToolkit
+{
+    protected $pdo;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;dbname=mi_base_de_datos';
+        $user = 'usuario';
+        $password = 'contraseña';
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function query($sql, $params = [])
+    {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function execute($sql, $params = [])
+    {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($params);
+    }
+
+    public function lastInsertId()
+    {
+        return $this->pdo->lastInsertId();
+    }
+
+    public function beginTransaction()
+    {
+        return $this->pdo->beginTransaction();
+    }
+
+    public function commit()
+    {
+        return $this->pdo->commit();
+    }
+
+    public function rollBack()
+    {
+        return $this->pdo->rollBack();
+    }
+}
